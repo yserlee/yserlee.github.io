c37f997b4db0fc09560bd73ffddb9bdf404820ee
Yunseok Lee <yser.lee@samsung.com>
2017-04-12 14:44:37 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <a className="navbar-brand" href="#">Home</a>
+                    </div>
+
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Tech</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+class NavbarContainer extends React.Component {
+    render() {
+        return <Navbar></Navbar>;
+    }
+}
+
+const element = <NavbarContainer></NavbarContainer>;
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
